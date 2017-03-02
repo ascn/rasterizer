845d610aa4ab088a83e851e985bf747f86d0ace0
Alexander Chan <alexanderchan97@gmail.com>
2017-03-02 16:48:18 -0500

    Add barycentric and flat shading

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 using namespace std;
 
 bool within(float y, float c1, float c2);
+float lerp(float a, float b, float alpha);
 
 typedef struct {
     vec4 vert[3];
@@ -23,6 +24,7 @@ typedef struct {
                           // [1][1] = max y
     bool is_renderable = true;
     pixel_t color;
+    vec4 normals[3];
 } face_t;
 
 typedef enum { NONE, WHITE, NORM_FLAT, NORM_GOURAUD, NORM_BARY,
@@ -30,8 +32,7 @@ typedef enum { NONE, WHITE, NORM_FLAT, NORM_GOURAUD, NORM_BARY,
 
 int main(int argc, char *argv[]) {
 
-    const float EPSILON = 0.0001;
-    e_shader shading;
+    e_shader shading = NONE;
 
     if (argc < 6) {
         fprintf(stderr, "usage: rasterize OBJ CAMERA WIDTH HEIGHT OUTPUT OPTION\n");
@@ -70,12 +71,12 @@ int main(int argc, char *argv[]) {
         z_buf.push_back(2);
     }
 
-    std::string mtl_path = "C:\Users\achan\Projects\cis560\rasterizer\rasterizer\obj";
+    std::string mtl_path = "../obj/";
 
     // LOAD OBJ
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
-    std::string err = tinyobj::LoadObj(shapes, materials, argv[1]);
+    std::string err = tinyobj::LoadObj(shapes, materials, argv[1], mtl_path.c_str());
     if ("" != err) {
     	cout << "error: " << err << endl;
     	exit(1);
@@ -102,8 +103,6 @@ int main(int argc, char *argv[]) {
 
     // READ CAMERA.TXT
     camera_mat_t camera = load_camera(argv[2]);
-    cout << camera.proj << endl;
-    cout << camera.view << endl;
 
     std::vector<face_t> faces;
     for (const auto &s : shapes) {
@@ -119,9 +118,18 @@ int main(int argc, char *argv[]) {
             tmp.vert[2] = vec4(mesh.positions[mesh.indices[i + 2] * 3],
                                mesh.positions[mesh.indices[i + 2] * 3 + 1],
                                mesh.positions[mesh.indices[i + 2] * 3 + 2], 1);
-            tmp.color = { materials[mesh.material_ids[i / 3]].diffuse[0] * 255,
-                          materials[mesh.material_ids[i / 3]].diffuse[1] * 255,
-                          materials[mesh.material_ids[i / 3]].diffuse[2] * 255 };
+            tmp.normals[0] = vec4(mesh.normals[mesh.indices[i] * 3],
+                                  mesh.normals[mesh.indices[i] * 3 + 1],
+                                  mesh.normals[mesh.indices[i] * 3 + 2], 0);
+            tmp.normals[1] = vec4(mesh.normals[mesh.indices[i + 1] * 3],
+                                  mesh.normals[mesh.indices[i + 1] * 3 + 1],
+                                  mesh.normals[mesh.indices[i + 1] * 3 + 2], 0);
+            tmp.normals[2] = vec4(mesh.normals[mesh.indices[i + 2] * 3],
+                                  mesh.normals[mesh.indices[i + 2] * 3 + 1],
+                                  mesh.normals[mesh.indices[i + 2] * 3 + 2], 0);
+            tmp.color = { (unsigned char) (materials[mesh.material_ids[i / 3]].diffuse[0] * 255),
+                          (unsigned char) (materials[mesh.material_ids[i / 3]].diffuse[1] * 255),
+                          (unsigned char) (materials[mesh.material_ids[i / 3]].diffuse[2] * 255) };
             faces.push_back(tmp);
         }
     }
@@ -146,12 +154,18 @@ int main(int argc, char *argv[]) {
         }
         if (f.pixel_coord[0][0] > f.pixel_coord[1][0]) {
             std::swap(f.pixel_coord[0], f.pixel_coord[1]);
+            std::swap(f.vert[0], f.vert[1]);
+            std::swap(f.normals[0], f.normals[1]);
         }
         if (f.pixel_coord[1][0] > f.pixel_coord[2][0]) {
             std::swap(f.pixel_coord[1], f.pixel_coord[2]);
+            std::swap(f.vert[1], f.vert[2]);
+            std::swap(f.normals[1], f.normals[2]);
         }
         if (f.pixel_coord[0][0] > f.pixel_coord[1][0]) {
             std::swap(f.pixel_coord[0], f.pixel_coord[1]);
+            std::swap(f.vert[0], f.vert[1]);
+            std::swap(f.normals[0], f.normals[1]);
         }
         f.bounding_box[0][0] = min(f.pixel_coord[0][0],
                                min(f.pixel_coord[1][0], f.pixel_coord[2][0]));
@@ -166,9 +180,9 @@ int main(int argc, char *argv[]) {
             f.is_renderable = false;
         }
         if (shading == RANDOM) {
-            f.color = { (float) std::rand() / RAND_MAX * 255,
-                        (float) std::rand() / RAND_MAX * 255,
-                        (float) std::rand() / RAND_MAX * 255 };
+            f.color = { (unsigned char) ((float) std::rand() / RAND_MAX * 255),
+                        (unsigned char) ((float) std::rand() / RAND_MAX * 255),
+                        (unsigned char) ((float) std::rand() / RAND_MAX * 255) };
         } else if (shading == NONE) {
 
         }
@@ -186,7 +200,7 @@ int main(int argc, char *argv[]) {
 
                 // Determine if intersects with pixel_coord[0], pixel_coord[1]
                 std::vector<float> intersects; // contains two x coordinates where the current face intersects with the row
-                bool horizontal = false;
+                std::vector<int> edge_intersects; // contains the indices of the vertices that are intersected
                 if (within(y, f.pixel_coord[0][1], f.pixel_coord[1][1])) {
                     // intersects with pixel[0], pixel[1]
                     // coordinates already sorted by increasing x
@@ -194,8 +208,6 @@ int main(int argc, char *argv[]) {
                         // vertical line
                         intersects.push_back(f.pixel_coord[0][0]);
                     } else if ((int) f.pixel_coord[0][1] == (int) f.pixel_coord[1][1]) {
-                        // horizontal line
-                        horizontal = true;
                         intersects.push_back(f.pixel_coord[0][0]);
                         intersects.push_back(f.pixel_coord[1][0]);
                     } else {
@@ -207,13 +219,14 @@ int main(int argc, char *argv[]) {
                         intersects.push_back((y + (m * f.pixel_coord[0][0]) -
                                                 f.pixel_coord[0][1]) / m);
                     }
+                    edge_intersects.push_back(0);
+                    edge_intersects.push_back(1);
                 }
 
                 if (within(y, f.pixel_coord[1][1], f.pixel_coord[2][1])) {
                     if ((int) f.pixel_coord[1][0] == (int) f.pixel_coord[2][0]) {
                         intersects.push_back(f.pixel_coord[1][0]);
                     } else if ((int) f.pixel_coord[1][1] == (int) f.pixel_coord[2][1]) {
-                        horizontal = true;
                         intersects.push_back(f.pixel_coord[1][0]);
                         intersects.push_back(f.pixel_coord[2][0]);
                     } else {
@@ -222,13 +235,14 @@ int main(int argc, char *argv[]) {
                         intersects.push_back((y + (m * f.pixel_coord[1][0]) -
                                                 f.pixel_coord[1][1]) / m);
                     }
+                    edge_intersects.push_back(1);
+                    edge_intersects.push_back(2);
                 }
 
                 if (within(y, f.pixel_coord[0][1], f.pixel_coord[2][1])) {
                     if ((int) f.pixel_coord[0][0] == (int) f.pixel_coord[2][0]) {
                         intersects.push_back(f.pixel_coord[2][0]);
                     } else if ((int) f.pixel_coord[0][1] == (int) f.pixel_coord[2][1]) {
-                        horizontal = true;
                         intersects.push_back(f.pixel_coord[0][0]);
                         intersects.push_back(f.pixel_coord[2][0]);
                     } else {
@@ -237,26 +251,16 @@ int main(int argc, char *argv[]) {
                         intersects.push_back((y + (m * f.pixel_coord[0][0]) -
                                                 f.pixel_coord[0][1]) / m);
                     }
+                    edge_intersects.push_back(0);
+                    edge_intersects.push_back(2);
                 }
 
                 std::sort(intersects.begin(), intersects.end());
-                // interpolate depth for intersects[0] and intersects[1]
-
-                /* Render wireframe */
-
-//                if (horizontal) {
-//                    for (int i = intersects[0]; i <= intersects[1]; ++i) {
-//                        (*out)(y, i) = f.color;
-//                    }
-//                }
-
-//                if (intersects[0] >= 0 && intersects[0] < w) {
-//                    (*out)(y, intersects[0]) = f.color;
-//                }
-//                if (intersects[1] >=0 && intersects[1] < w) {
-//                    (*out)(y, intersects[1]) = f.color;
-//                }
-
+                if (intersects[0] > intersects[1]) {
+                    std::swap(intersects[0], intersects[1]);
+                    std::swap(edge_intersects[0], edge_intersects[2]);
+                    std::swap(edge_intersects[1], edge_intersects[3]);
+                }
                 // y * h + i
                 double x1 = f.pixel_coord[0][0];
                 double x2 = f.pixel_coord[1][0];
@@ -268,24 +272,95 @@ int main(int argc, char *argv[]) {
                     if (i < 0 || i > w) { continue; }
                     // Get barycentric coordinate of pixel
                     double x = i;
-                    double l0 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
-                    double l1 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
+                    double l0 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) /
+                                ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
+                    double l1 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) /
+                                ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
                     double l2 = 1 - l0 - l1;
-                    double pix_depth = (1 / f.vert[0][2]) * l0 + (1 / f.vert[1][2]) * l1 + (1 / f.vert[2][2]) * l2;
+                    double pix_depth = (1 / f.vert[0][2]) * l0 +
+                                       (1 / f.vert[1][2]) * l1 +
+                                       (1 / f.vert[2][2]) * l2;
                     pix_depth = 1 / pix_depth;
                     if (pix_depth < z_buf[y * w + i] && within(pix_depth, 0, 1)) {
                         switch (shading) {
                         case NONE:
-
-                            (*out)(y, i) = { f.color.r * pix_depth,
-                                             f.color.g * pix_depth,
-                                             f.color.b * pix_depth };
+                            (*out)(y, i) = { f.color.r * (1 / pix_depth),
+                                             f.color.g * (1 / pix_depth),
+                                             f.color.b * (1 / pix_depth) };
                             break;
                         case WHITE:
                             (*out)(y, i) = { 255, 255, 255 };
                             break;
+                        case NORM_FLAT: {
+                            vec4 view_n_1 = camera.view * f.normals[0];
+                            (*out)(y, i) = { (unsigned char) ((view_n_1[0] + 1) * 127.5),
+                                             (unsigned char) ((view_n_1[1] + 1) * 127.5),
+                                             (unsigned char) ((view_n_1[2] + 1) * 127.5) };
+                            break;
+                        }
+                        case NORM_GOURAUD: {
+                            vec4 view_n_1 = camera.view * f.normals[0];
+                            vec4 view_n_2 = camera.view * f.normals[1];
+                            vec4 view_n_3 = camera.view * f.normals[2];
+                            pixel_t c[3];
+                            c[0] = { (unsigned char) ((view_n_1[0] + 1) * 127.5),
+                                     (unsigned char) ((view_n_1[1] + 1) * 127.5),
+                                     (unsigned char) ((view_n_1[2] + 1) * 127.5) };
+                            c[1] = { (unsigned char) ((view_n_2[0] + 1) * 127.5),
+                                     (unsigned char) ((view_n_2[1] + 1) * 127.5),
+                                     (unsigned char) ((view_n_2[2] + 1) * 127.5) };
+                            c[2] = { (unsigned char) ((view_n_3[0] + 1) * 127.5),
+                                     (unsigned char) ((view_n_3[1] + 1) * 127.5),
+                                     (unsigned char) ((view_n_3[2] + 1) * 127.5) };
+                            // find distance of y
+                            float r1 = lerp(c[edge_intersects[0]].r,
+                                           c[edge_intersects[1]].r,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            float g1 = lerp(c[edge_intersects[0]].g,
+                                           c[edge_intersects[1]].g,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            float b1 = lerp(c[edge_intersects[0]].b,
+                                           c[edge_intersects[1]].b,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            float r2 = lerp(c[edge_intersects[2]].r,
+                                           c[edge_intersects[3]].r,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            float g2 = lerp(c[edge_intersects[2]].g,
+                                           c[edge_intersects[3]].g,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            float b2 = lerp(c[edge_intersects[2]].b,
+                                           c[edge_intersects[3]].b,
+                                           std::abs(f.pixel_coord[edge_intersects[0]][1] - y));
+                            (*out)(y, i) = { (unsigned char) lerp(r1, r2, intersects[1] - i), 
+                                             (unsigned char) lerp(g1, g2, intersects[1] - i),
+                                             (unsigned char) lerp(b1, b2, intersects[1] - i) };
+                            break;
+                        }
+                        case NORM_BARY: {
+                            vec4 view_n_1 = camera.view * f.normals[0];
+                            vec4 view_n_2 = camera.view * f.normals[1];
+                            vec4 view_n_3 = camera.view * f.normals[2];
+                            pixel_t c1 = { (unsigned char) ((view_n_1[0] + 1) * 127.5),
+                                           (unsigned char) ((view_n_1[1] + 1) * 127.5),
+                                           (unsigned char) ((view_n_1[2] + 1) * 127.5) };
+                            pixel_t c2 = { (unsigned char) ((view_n_2[0] + 1) * 127.5),
+                                           (unsigned char) ((view_n_2[1] + 1) * 127.5),
+                                           (unsigned char) ((view_n_2[2] + 1) * 127.5) };
+                            pixel_t c3 = { (unsigned char) ((view_n_3[0] + 1) * 127.5),
+                                           (unsigned char) ((view_n_3[1] + 1) * 127.5),
+                                           (unsigned char) ((view_n_3[2] + 1) * 127.5) };
+                            (*out)(y, i) = { c1.r * l0 + c2.r * l1 + c3.r * l2, 
+                                             c1.g * l0 + c2.g * l1 + c3.g * l2,
+                                             c1.b * l0 + c2.b * l1 + c3.b * l2 };
+                            break;
+                        }
+                        case NORM_GOURAUD_Z:
+                        case NORM_BARY_Z:
                         case RANDOM:
                             (*out)(y, i) = f.color;
+                            break;
+                        default:
+                            fprintf(stderr, "error: option not handled\n");
                             break;
                         }
                         //(*out)(y, i) = f.color;
@@ -305,4 +380,8 @@ int main(int argc, char *argv[]) {
 
 bool within(float y, float c1, float c2) {
     return (c1 <= y && y <= c2) || (c2 <= y && y <= c1);
+}
+
+float lerp(float a, float b, float alpha) {
+    return a + alpha * (b - a);
 }
